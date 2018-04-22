ef01c42c64d5495d31140a520e5ba4b36ef047bd
许嘉琰 <ahcigar@foxmail.com>
2018-04-22 13:49:34 +0800

    suf2ts_M

--- suf2ts_M.py
+++ suf2ts_M.py
@@ -60,59 +60,98 @@ def cal_CH4_xyz(CONTCAR, theta= 109.5/2, \
     s2_abO = (abc.T*abO).T - (abc.T*s2).T 
     s3_abO = (abc.T*abO).T - (abc.T*s3).T
     abO_s1 = (abc.T*s1).T - (abc.T*abO).T 
-    ###
+    ### e1, e2, e22, e3 1x3
     e1 = (s2_abO + s3_abO) / np.linalg.norm(s2_abO + s3_abO)
+    e1 = e1.sum(0)
     e2 = abO_s1 / np.linalg.norm(abO_s1)
-    e1dote2 = np.dot(e1.sum(0), e2.sum(0).T)
+    e2 = e2.sum(0)
+    e1dote2 = np.dot(e1, e2.T)
     ##
-    y_e2 = np.sqrt(1.0**2/(1-e1dote2**2))
-    x_e1 = -y_e2*e1dote2
+    y_e2 = np.sqrt(1.0**2*(1-np.cos(np.pi/2)**2)/(1-e1dote2**2))
+    x_e1 = 1.0*np.cos(np.pi/2) - y_e2*e1dote2
     e22 = x_e1*e1 + y_e2*e2
+    e22 = e22/np.linalg.norm(e22)
+    ##
+    e3 = np.dot(np.linalg.inv(np.vstack((np.vstack((e1, e22)) ,[1, 0, 0]))), \
+            [0, 0, 1])
+    e3 = e3.T/np.linalg.norm(e3)
     ### set abO as O , e1 as x e22 as y 
+    #    H1
+    #   /  \  H3 H4
+    #  /    \ / /
+    # O      C
+    #  \     /\
+    #   \---M  H2
     ## get M
-    rad_e1Oe2 = np.arccos(np.dot(e1.sum(0), e2.sum(0).T))
-    M_e1x = np.sqrt(np.linalg.norm(abO_s1)**2/(1+np.tan(rad_e1Oe2)**2))
-    M_e22y = M_e1x*np.tan(rad_e1Oe2)
+    rad_e1Oe2 = np.arccos(np.dot(e1, e2.T))
+    M_e1x = np.linalg.norm(abO_s1)*np.cos(rad_e1Oe2) 
+    M_e22y = np.linalg.norm(abO_s1)*np.sin(rad_e1Oe2)
     ## get H1
     H1_e1x = np.sqrt(OH_distance**2/(1+np.tan(theta/180*np.pi)**2))
     H1_e22y = H1_e1x*np.tan(theta/180*np.pi)
-    H1_XYZ = (abc.T*abO).T - H1_e1x*e1 + H1_e22y*e22
+    H1_XYZ = (abc.T*abO).T.sum(0) + H1_e1x*e1 + H1_e22y*e22
     ## get C
     ## set x = ay + b
     #  set uy^2 + vy + w = 0
     a = -(H1_e22y-M_e22y)/(H1_e1x-M_e1x)
-    b = (OH_distance**2-np.linalg.norm(abO_s1)**2-(H1_e1x**2-M_e1x**2)-(H1_e22y**2-M_e22y**2))/\
+    b = (H1C_distance**2-CM_distance**2-(H1_e1x**2-M_e1x**2)-(H1_e22y**2-M_e22y**2))/\
             (-2*(H1_e1x-M_e1x))
     u = a**2+1
     v = 2*a*b-2*a*H1_e1x-2*H1_e22y
     w = b**2-2*b*H1_e1x+H1_e1x**2+H1_e22y**2-OH_distance**2
-    M_e1x = (-v+np.sqrt(v**2-4*u*w))/(2*u)
+    C_e22y = (-v+np.sqrt(v**2-4*u*w))/(2*u) # choose the bigger one
+    C_e1x = a*C_e22y + b
+    C_XYZ = (abc.T*abO).T.sum(0) + C_e1x*e1 + C_e22y*e22
+    ## get H2, H3, H4
+    H34_degree = 15
+    H_proj = np.cos(H34_degree/180*np.pi)*e1 - np.sin(H34_degree/180*np.pi)*e22
+    H_proj = H_proj/np.linalg.norm(H_proj) # set 15 degree bt e1
+    H3_XYZ = 1.0*np.tan(109.5/2/180*np.pi)*e3 + H_proj
+    H3_XYZ = C_XYZ + H3_XYZ/np.linalg.norm(H3_XYZ)*1.096
+    H4_XYZ = - 1.0*np.tan(109.5/2/180*np.pi)*e3 + H_proj
+    H4_XYZ = C_XYZ + H4_XYZ/np.linalg.norm(H4_XYZ)*1.096 
     ##
+    H2_XYZ = 1.096*np.cos(((360-109.5)/2-H34_degree)/180*np.pi)*e1 + 1.096*np.sin(95.25/180*np.pi)*e22
+    H2_XYZ = C_XYZ + H2_XYZ/np.linalg.norm(H2_XYZ)*1.096
     ###
+    H1_xyz = (np.dot(np.linalg.inv(abc.T), H1_XYZ)).T
+    H2_xyz = (np.dot(np.linalg.inv(abc.T), H2_XYZ)).T
+    H3_xyz = (np.dot(np.linalg.inv(abc.T), H3_XYZ)).T
+    H4_xyz = (np.dot(np.linalg.inv(abc.T), H4_XYZ)).T
+    C_xyz = (np.dot(np.linalg.inv(abc.T), C_XYZ)).T
+    return H1_xyz, H2_xyz, H3_xyz, H4_xyz, C_xyz
+###
+def create_fort188(dir, suf_dir, ts_dir):
+    with open(os.path.join(ts_dir, 'fort.188'), 'w') as f:
+        f.write('1\n3\n6\n4\n0.04\n1   5     1.4\n0') # 1.4 H1C_distance
 
 ###
-def create_POSCAR(dir, suf_dir, Hab_dir):
+def create_POSCAR(dir, suf_dir, ts_dir):
     CONTCAR = os.path.join(suf_dir, 'CONTCAR')
-    H_xyz = cal_CH4_xyz(CONTCAR)
+    H1_xyz, H2_xyz, H3_xyz, H4_xyz, C_xyz = cal_CH4_xyz(CONTCAR)
     content = []
-    shutil.copyfile(os.path.join(suf_dir, 'CONTCAR'), os.path.join(Hab_dir, 'POSCAR'))
-    with open(os.path.join(Hab_dir, 'POSCAR'), 'r') as f:
+    shutil.copyfile(os.path.join(suf_dir, 'CONTCAR'), os.path.join(ts_dir, 'POSCAR'))
+    with open(os.path.join(ts_dir, 'POSCAR'), 'r') as f:
         content = f.readlines()
     new_content = ''
     for i in range(5):
         new_content += content[i]
-    new_content += '{:>5}'.format('H') + str(content[5])
-    new_content += '{:>5}'.format('1') + str(content[6])
+    new_content += '{:>5}{:>5}'.format('H', 'C') + str(content[5])
+    new_content += '{:>5}{:>5}'.format('4', '1') + str(content[6])
     for line in content[7:9]:
         new_content += line
-    new_content += '  {:<14}  {:<14}  {:<14}   T   T   T\n'.format(H_xyz[0], H_xyz[1], H_xyz[2])
+    new_content += '  {:<14}  {:<14}  {:<14}   T   T   T\n'.format(H1_xyz[0], H1_xyz[1], H1_xyz[2])
+    new_content += '  {:<14}  {:<14}  {:<14}   T   T   T\n'.format(H2_xyz[0], H2_xyz[1], H2_xyz[2])
+    new_content += '  {:<14}  {:<14}  {:<14}   T   T   T\n'.format(H3_xyz[0], H3_xyz[1], H3_xyz[2])
+    new_content += '  {:<14}  {:<14}  {:<14}   T   T   T\n'.format(H4_xyz[0], H4_xyz[1], H4_xyz[2])
+    new_content += '  {:<14}  {:<14}  {:<14}   T   T   T\n'.format(C_xyz[0], C_xyz[1], C_xyz[2])
     for line in content[9:58]:
         new_content += line
-    with open(os.path.join(Hab_dir, 'POSCAR'), 'w') as f:
+    with open(os.path.join(ts_dir, 'POSCAR'), 'w') as f:
         f.write(new_content)
 ###
-def prepare_ab(work_dir, finished_dirs):
-    result_path = os.path.join(work_dir, 'prepare_Hab_sp2.xu')
+def prepare_ts(work_dir, finished_dirs):
+    result_path = os.path.join(work_dir, 'prepare_ts_M.xu')
     if not os.path.exists(result_path):
         print('Start!')
     else:
@@ -122,40 +161,56 @@ def prepare_ab(work_dir, finished_dirs):
     ###
     for dir in finished_dirs:
         suf_dir = os.path.join(dir, 'suf')
-        ab_dir = os.path.join(dir, 'Hab_sp2')
-        if os.path.exists(ab_dir):
-            if not os.path.exists(os.path.join(ab_dir, 'print-out')):
-                shutil.rmtree(ab_dir)
-                os.system(r'echo %s rming and making ab >> %s' %(ab_dir, result_path))
-                os.mkdir(ab_dir)
-                mC.create_VASPsp(dir, suf_dir, ab_dir)
-                mC.create_INCAR(dir, suf_dir, ab_dir, '2')
-                shutil.copyfile(os.path.join(suf_dir, 'KPOINTS'), os.path.join(ab_dir, 'KPOINTS'))
-                create_POSCAR(dir, suf_dir, ab_dir)
-                mC.create_POTCAR(ab_dir, './data/potpaw_PBE.54')
+        ts_dir = os.path.join(dir, 'ts')
+        if os.path.exists(ts_dir):
+            print(ts_dir)
+            if not os.path.exists(os.path.join(ts_dir, 'print-out')):
+                shutil.rmtree(ts_dir)
+                os.system(r'echo %s rming and making ts >> %s' %(ts_dir, result_path))
+                os.mkdir(ts_dir)
+                mC.create_VASPsp(dir, suf_dir, ts_dir)
+                mC.create_INCAR(dir, suf_dir, ts_dir, '1')
+                shutil.copyfile(os.path.join(suf_dir, 'KPOINTS'), os.path.join(ts_dir, 'KPOINTS'))
+                create_POSCAR(dir, suf_dir, ts_dir)
+                create_fort188(dir, suf_dir, ts_dir)
+                mC.create_POTCAR(ts_dir, './data/potpaw_PBE.54')
         else:
-            os.system(r'echo %s making ab >> %s' %(ab_dir, result_path))
-            os.mkdir(ab_dir)
-            mC.create_VASPsp(dir, suf_dir, ab_dir)
-            mC.create_INCAR(dir, suf_dir, ab_dir, '2')
-            shutil.copyfile(os.path.join(suf_dir, 'KPOINTS'), os.path.join(ab_dir, 'KPOINTS'))
-            create_POSCAR(dir, suf_dir, ab_dir)
-            mC.create_POTCAR(ab_dir, './data/potpaw_PBE.54')
+            print(ts_dir)
+            os.system(r'echo %s making ts >> %s' %(ts_dir, result_path))
+            os.mkdir(ts_dir)
+            mC.create_VASPsp(dir, suf_dir, ts_dir)
+            mC.create_INCAR(dir, suf_dir, ts_dir, '1')
+            shutil.copyfile(os.path.join(suf_dir, 'KPOINTS'), os.path.join(ts_dir, 'KPOINTS'))
+            create_POSCAR(dir, suf_dir, ts_dir)
+            create_fort188(dir, suf_dir, ts_dir)
+            mC.create_POTCAR(ts_dir, './data/potpaw_PBE.54')
 
 ###
 ##
 ###
 def main():
-    work_dir = []
     if len(sys.argv) == 1:
         work_dir = r'./data/dopRutile'
+        finished_dirs = mW.check_printout(work_dir, 'suf')
+        print(finished_dirs)
+        prepare_ts(work_dir, finished_dirs)       
     elif len(sys.argv) == 2:
+        work_dir = sys.argv[1] 
+        finished_dirs = mW.check_printout(work_dir, 'suf')
+        prepare_ts(work_dir, finished_dirs)       
+    elif len(sys.argv) == 3:
         work_dir = sys.argv[1]
-        if not os.path.isdir(work_dir):
-            sys.exit()
+        element = sys.argv[2]
+        finished_dirs = []
+        converg_dirs = mW.check_printout(work_dir, 'suf')
+        for vasp_dir in converg_dirs:
+            if re.match(r'.*' + element + r'O2_.*', vasp_dir):
+                finished_dirs.append(vasp_dir)
+        prepare_ts(work_dir, finished_dirs)       
+    else:
+        print('Wrong argvs!')
+        sys.exit()
     ###
-    finished_dirs = mW.check_printout(work_dir, 'suf')
-    prepare_ab(work_dir, finished_dirs)       
 ###
 if __name__ == '__main__':
     main()
